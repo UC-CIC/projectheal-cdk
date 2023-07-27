ebab1b7e31bbdc4ab580a158c1a6e536ff7f0169
Chris Robinson <awscrob@amazon.com>
2023-07-26 23:03:35 -0700

    Minor corrections

--- pitt_comm_cdk/lambda/bedrock_handler/index.py
+++ pitt_comm_cdk/lambda/bedrock_handler/index.py
@@ -21,26 +21,45 @@ class JSONEncoder(json.JSONEncoder):
         if isinstance(obj, Decimal):
             return float(obj)
         return json.JSONEncoder.default(self, obj)
-    
+
+class Titan():
+    def prompt_engineer_baseline_twitter( counteract, audience ):
+        prompt = 'Write a tweet As a Public Health Administrator for the ' + audience + '. The Tweet should contain only truthful information counteracting the claim "' + counteract + '". Confine it to 140 characters and include hashtags. Do not include harmful content.'
+        return prompt
+    def prompt_engineer_update_twitter( previous, prompt ):
+        prompt = 'Given the following tweet: "' + previous + '", rewrite it while still maintaining 140 characters and including hashtags to reflect the following request: "' + prompt + '". Do not include harmful content.'
+        return prompt
+
 def handler(event,context):
     print(boto3.__version__)
     print(botocore.__version__)
 
-    
-
     # Generation Data
     try:
         field_values=json.loads(event["body"])
+        print(field_values)
+        
         counteracts=field_values["counteract"]
         audiences=field_values["audiences"]
         platforms=field_values["platforms"]
+        mode=field_values["mode"]
+        previous_prompt=field_values["previous_prompt"]
+        new_prompt=field_values["prompt"]
     except:
         print("Fail safe generation executed")
         counteracts=["Only gay men can get Mpox"]
         audiences=["General Public"]
         platforms=["Twitter"]
+        mode="baseline"
+        previous_prompt=""
+        new_prompt=""
 
-
+    print(counteracts)
+    print(audiences)
+    print(platforms)
+    print(mode)
+    print(previous_prompt)
+    print(new_prompt)
     
     combos_choices = [counteracts,audiences,platforms]
     combos=list(itertools.product(*combos_choices))
@@ -52,14 +71,23 @@ def handler(event,context):
         counteract=combo[0]
         audience=combo[1]
         platform=combo[2]
+
+        prompt = ""
+        if platform == "Twitter":
+            if mode == "baseline":
+                prompt = Titan.prompt_engineer_baseline_twitter( counteract, audience )
+            else:
+                prompt = Titan.prompt_engineer_update_twitter( previous_prompt, new_prompt )
+
+        print("Prompt: " + prompt + "\n")
         bedrock_payload = json.dumps(
             {
-                "inputText": "what is 5 multiplied by 5",
+                "inputText": prompt,
                 "textGenerationConfig": {
                     "maxTokenCount": 512,
                     "stopSequences": [],
                     "temperature": 0,
-                    "topP": 0.9
+                    "topP": 0.8
                 } 
             }
         )
@@ -70,9 +98,10 @@ def handler(event,context):
         response = bedrock.invoke_model(body=bedrock_payload, modelId=modelId, accept=accept, contentType=contentType)
         response_body =  json.loads(response.get('body').read())
         print( response_body )
-        # short circuit
+        responses.append(response_body.get('results')[0].get('outputText'))
+        # short circuit for fail safe
         i += 1
-        if i > 5:
+        if i > 3:
             break
 
     
@@ -81,5 +110,5 @@ def handler(event,context):
     return {
         "statusCode":200,
         "headers": CORS_HEADERS,
-        "body": json.dumps({},cls=JSONEncoder)
+        "body": json.dumps(responses,cls=JSONEncoder)
     }
--- pitt_comm_cdk/pitt_comm_cdk_stack.py
+++ pitt_comm_cdk/pitt_comm_cdk_stack.py
@@ -5,6 +5,7 @@ from aws_cdk import (
     Stack,
     aws_lambda as lambda_,
     Duration,
+    aws_apigateway as apigateway,
     aws_iam as iam
 )
 
@@ -40,7 +41,6 @@ class PittCommCdkStack(Stack):
             )
         )
 
-
         fn_bedrock_handler = lambda_.Function(
             self,"fn-bedrock-handler",
             description="fn-bedrock-handler", #microservice tag
@@ -63,3 +63,51 @@ class PittCommCdkStack(Stack):
             )             
             ]
         ))
+        bedrock_lambda_role.attach_inline_policy(iam.Policy(self, "bedrock-basic-execution-logging",
+            statements=[iam.PolicyStatement(
+                actions=["logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents"],
+                resources=["*"]
+            )             
+            ]
+        ))
+        core_api = apigateway.RestApi(
+            self,"core-api",
+            endpoint_configuration=apigateway.EndpointConfiguration(
+                types=[apigateway.EndpointType.REGIONAL]
+            ),
+            default_cors_preflight_options=apigateway.CorsOptions(
+                allow_methods=['GET', 'OPTIONS','PUT','PATCH','POST'],
+                allow_origins=[FULL_CFRONT_URL, LOCALHOST_ORIGIN if ALLOW_LOCALHOST_ORIGIN else ""])
+        )
+
+        ###### Route Base = /api [match for cloud front purposes]
+        api_route = core_api.root.add_resource("api")
+
+        # /generate/communication
+
+        # /generate/
+        public_route_generate=api_route.add_resource("generate")
+        # /generate/communication
+        public_route_communication=public_route_generate.add_resource("communication")
+        # POST: /generate/communication
+        communication_post_integration=apigateway.LambdaIntegration(fn_bedrock_handler)
+        method_uploader_post=public_route_communication.add_method(
+            "POST",communication_post_integration,
+            api_key_required=True
+        )       
+
+
+        #################################################################################
+        # Usage plan and api key to "lock" API to only CFRONT calls (or local in our case)
+        #################################################################################
+        plan = core_api.add_usage_plan(
+            "UsagePlan",name="public plan",
+            throttle=apigateway.ThrottleSettings(
+                rate_limit=10,
+                burst_limit=2
+            )
+        )
+
+        core_key=core_api.add_api_key("core-api-key")
+        plan.add_api_key(core_key)
+        plan.add_api_stage(api=core_api,stage=core_api.deployment_stage)
