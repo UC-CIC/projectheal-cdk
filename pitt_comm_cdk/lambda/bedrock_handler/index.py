f84e51232aaf340136625bb873840dd3ecc82fc3
Chris Robinson <awscrob@amazon.com>
2023-07-31 22:15:28 -0700

    Prompt engineering updated

--- pitt_comm_cdk/lambda/bedrock_handler/index.py
+++ pitt_comm_cdk/lambda/bedrock_handler/index.py
@@ -14,7 +14,8 @@ CORS_HEADERS = {
 }
 
 bedrock = boto3.client('bedrock' , 'us-east-1')
-
+TEMPERATURE=.1
+TOP_P=.9
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -24,11 +25,39 @@ class JSONEncoder(json.JSONEncoder):
 
 class Titan():
     def prompt_engineer_baseline_twitter( counteract, audience ):
-        prompt = 'Write a tweet As a Public Health Administrator for the ' + audience + '. The Tweet should contain only truthful information counteracting the claim "' + counteract + '". Confine it to 140 characters and include hashtags. Do not include harmful content.'
-        return prompt
-    def prompt_engineer_update_twitter( previous, prompt ):
-        prompt = 'Given the following tweet between <<" & ">> <<"' + previous + '">>. As a smart assistant, rewrite the tweet according to the following instructions: "' + prompt + '". Do not include harmful content.'
-        return prompt
+        prompt = "You are a nurse needing to combat incorrect information.\n"
+        prompt += "You have been given the following list of incorrect statements:\n" + counteract + "\n\n"
+        prompt += "As a nurse, write a single Tweet that provides truthful information instead for the following audience: " + audience + "\n\n"
+        prompt += "The created Tweet should contain three hashtags and not include harmful content."
+        return prompt.replace("\n", " ")
+    def prompt_engineer_baseline_blog( counteract, audience ):
+        prompt = "You are a nurse needing to combat incorrect information.\n"
+        prompt += "You have been given the following list of incorrect statements:\n" + counteract + "\n\n"
+        prompt += "As a nurse, write a single blog post that provides truthful information instead for the following audience" + audience + "\n\n"
+        prompt += "The created blog post should be no longer than three paragraphs and not include harmful content."
+        return prompt.replace("\n", " ")
+    def prompt_engineer_baseline_reddit( counteract, audience ):
+        prompt = "You are a health professional needing to combat incorrect information.\n"
+        prompt += "You have been given the following list of incorrect statements:\n" + counteract + "\n\n"
+        prompt += "As a nurse, write a single Reddit post that provides truthful information instead for the following audience" + audience + "\n\n"
+        prompt += "The created Reddit post should include a TLDR at the end and not include harmful content."
+        return prompt.replace("\n", " ")
+    
+    def prompt_engineer_update_twitter( previous, promp_instructions ):
+        prompt = "As a nurse you read the following Tweet: " + previous + "\n\n"
+        prompt += "You want to update the Tweet according to the following instruction:" + promp_instructions + "\n\n"
+        prompt += "Answer with the update and do not include harmful content."
+        return prompt.replace("\n", " ")
+    def prompt_engineer_update_blog( previous, promp_instructions ):
+        prompt = "As a nurse you read the following blog post: " + previous + "\n\n"
+        prompt += "You want to update the Tweet according to the following instruction:" + promp_instructions + "\n\n"
+        prompt += "Answer with the update and do not include harmful content."
+        return prompt.replace("\n", " ")
+    def prompt_engineer_update_reddit( previous, promp_instructions ):
+        prompt = "As a nurse you read the following Reddit post: " + previous + "\n\n"
+        prompt += "You want to update the Tweet according to the following instruction:" + promp_instructions + "\n\n"
+        prompt += "Answer with the update and do not include harmful content."
+        return prompt.replace("\n", " ")    
 
 def handler(event,context):
     print(boto3.__version__)
@@ -46,7 +75,7 @@ def handler(event,context):
         previous_prompt=field_values["previous_prompt"]
         new_prompt=field_values["prompt"]
     except:
-        print("Fail safe generation executed")
+        print("<<<Fail safe generation executed>>>")
         counteracts=["Only gay men can get Mpox"]
         audiences=["General Public"]
         platforms=["Twitter"]
@@ -54,58 +83,62 @@ def handler(event,context):
         previous_prompt=""
         new_prompt=""
 
+    '''
     print(counteracts)
     print(audiences)
     print(platforms)
     print(mode)
     print(previous_prompt)
     print(new_prompt)
+    '''
     
-    combos_choices = [counteracts,audiences,platforms]
-    combos=list(itertools.product(*combos_choices))
 
-    responses = []
+    responses=[]
 
-    i = 1
-    for combo in combos:
-        counteract=combo[0]
-        audience=combo[1]
-        platform=combo[2]
+    counteract=""
+    for count,entry in enumerate(counteracts):
+        counteract += str(count+1) + ") " + entry + "\n"
+    print(counteract)
+    audience=audiences[0]
+    platform=platforms[0]
 
-        prompt = ""
+    prompt = ""
+    if mode == "baseline":
         if platform == "Twitter":
-            if mode == "baseline":
-                prompt = Titan.prompt_engineer_baseline_twitter( counteract, audience )
-            elif mode == "update":
-                prompt = Titan.prompt_engineer_update_twitter( previous_prompt, new_prompt )
+            prompt = Titan.prompt_engineer_baseline_twitter( counteract, audience )
+        elif platform == "Blog Post":
+            prompt = Titan.prompt_engineer_baseline_blog( counteract, audience )
+        elif platform == "Reddit":
+            prompt = Titan.prompt_engineer_baseline_reddit( counteract, audience )
+    elif mode == "update":
+        if platform == "Twitter":
+            prompt = Titan.prompt_engineer_update_twitter( previous_prompt, new_prompt )
+        elif platform == "Blog Post":
+            prompt = Titan.prompt_engineer_update_blog( previous_prompt, new_prompt )
+        elif platform == "Reddit":
+            prompt = Titan.prompt_engineer_update_reddit( previous_prompt, new_prompt )
 
-        print("Prompt: " + prompt + "\n")
-        bedrock_payload = json.dumps(
-            {
-                "inputText": prompt,
-                "textGenerationConfig": {
-                    "maxTokenCount": 512,
-                    "stopSequences": [],
-                    "temperature": .1,
-                    "topP": 1
-                } 
-            }
-        )
-        modelId = 'amazon.titan-tg1-large' # change this to use a different version from the model provider
-        accept = 'application/json'
-        contentType = 'application/json'
 
-        response = bedrock.invoke_model(body=bedrock_payload, modelId=modelId, accept=accept, contentType=contentType)
-        response_body =  json.loads(response.get('body').read())
-        print( response_body )
-        responses.append(response_body.get('results')[0].get('outputText').strip())
-        # short circuit for fail safe
-        i += 1
-        if i > 3:
-            break
+    print("Prompt: " + prompt + "\n")
+    bedrock_payload = json.dumps(
+        {
+            "inputText": prompt,
+            "textGenerationConfig": {
+                "maxTokenCount": 512,
+                "stopSequences": [],
+                "temperature": TEMPERATURE,
+                "topP": TOP_P
+            } 
+        }
+    )
+    modelId = 'amazon.titan-tg1-large' # change this to use a different version from the model provider
+    accept = 'application/json'
+    contentType = 'application/json'
 
-    
-        
+    response = bedrock.invoke_model(body=bedrock_payload, modelId=modelId, accept=accept, contentType=contentType)
+    response_body =  json.loads(response.get('body').read())
+    print( response_body )
+    responses.append(response_body.get('results')[0].get('outputText').strip())
 
     return {
         "statusCode":200,
